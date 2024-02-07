41b431d94f2a53ff2a4af9985e3138bec1ced263
OUISSAL EL HICHOU <114773271+ouissalhichou1@users.noreply.github.com>
2024-02-08 00:45:25 +0100

    faild

--- src/MainServer/MainServer.java
+++ src/MainServer/MainServer.java
@@ -3,7 +3,7 @@ package MainServer;
 import java.io.FileInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.rmi.RemoteException; // Correct import statement
+import java.rmi.RemoteException;
 import java.util.Properties;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -13,7 +13,7 @@ import RessourcesForRMI.SlaveDataList;
 public class MainServer extends Thread {
     private static Executor executor;
     private static TaskQueue taskQueue;
-    private static int TaskID = 100;
+    private static int TaskID = 0;
     static int MainServer_port;
     static String MainServer_host;
 
@@ -42,56 +42,61 @@ public class MainServer extends Thread {
             ip = new FileInputStream(FileConfiguration);
             prop.load(ip);
         } catch (Exception e2) {
-            System.exit(0);
+            System.err.println("Error loading properties file: " + e2.getMessage());
+            e2.printStackTrace();
+            System.exit(1);
         }
+
         /*
          * Run the socket of Worker
          */
         MainServer_port = Integer.parseInt(prop.getProperty("MainServer.port"));
         MainServer_host = prop.getProperty("MainServer.host");
-        // Creating an object of ServerSocket class
-        // in the main() method for socket connection
 
         /*
-         * *
          * GET SERVERS
          */
-        getSelvers(prop);
-        System.out.println("Number of Workers :" + slaveDataList.ListWorkers.size());
+        getSlaves(prop);
+        System.out.println("Number of Slaves: " + slaveDataList.ListWorkers.size());
 
-        ServerSocket ss;
+        ServerSocket ss = null;
         try {
             ss = new ServerSocket(MainServer_port);
+            ss.setReuseAddress(true); // Set socket options before binding
 
             executor = Executors.newFixedThreadPool(10);
             taskQueue = new TaskQueue();
 
-            // ? Make it nonblocking
-            ss.setReuseAddress(true);
-            ss.setSoTimeout(0);
+            System.out.println("Server started at port " + MainServer_port);
 
-            System.out.println("Started at port " + MainServer_port);
-
-            // !Lunch Mini Workers Threads executing the Queue
+            // Launch Mini Workers Threads executing the Queue
             for (int i = 0; i < 7; i++) {
                 executor.execute(new ServerThread(taskQueue));
             }
 
-            // !Listen to client
+            // Listen to clients
             while (true) {
                 // Accept the socket from the client
                 Socket soc = ss.accept();
-                System.out.println("+(" + TaskID + ") : New Task in queue");
+                System.out.println("New Task in queue: " + TaskID);
                 Task newTask = new Task(soc, TaskID++);
                 taskQueue.add(newTask);
             }
 
         } catch (Exception e1) {
             e1.printStackTrace();
+        } finally {
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-    public static int getSelvers(Properties prop) {
+    public static int getSlaves(Properties prop) {
         String hostSlv = prop.getProperty("Worker1.host");
         String portSlv = prop.getProperty("Worker1.port");
         int i = 1;
@@ -104,5 +109,4 @@ public class MainServer extends Thread {
         }
         return i - 1;
     }
-
 }
