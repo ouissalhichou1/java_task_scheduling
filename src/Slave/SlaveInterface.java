c331b0344f86e96e70c724da4c6304ad92d0a35d
OUISSAL EL HICHOU <114773271+ouissalhichou1@users.noreply.github.com>
2024-03-04 23:32:58 +0100

    final

--- src/Slave/SlaveInterface.java
+++
@@ -1,9 +0,0 @@
-package Slave;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-
-public interface SlaveInterface extends Remote {
-    ArrayList<Integer> performOperation(ArrayList<Integer> data) throws RemoteException;
-}
